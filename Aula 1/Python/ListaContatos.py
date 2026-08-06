b66f6e8255fb0174d31583c8d263ff0ee95a886d
Dione Pinheiro <dionepinheiro6030@gmail.com>
2026-08-06 15:57:26 -0300

    Renomear variável de dicionário para Contatos e implementar funções de listar e pesquisar contatos

--- Aula 1/Python/ListaContatos.py
+++ Aula 1/Python/ListaContatos.py
@@ -1,4 +1,4 @@
-dicionario = {
+Contatos = {
     "Yago": "991935134",
     "Diego": "991987654",
     "Pedro": "991123456",
@@ -6,7 +6,28 @@ dicionario = {
     "Paulo": "99512619"
     }
 
+def listar():
+    print(Contatos)
+
+def pesquisar(nome):
+    print(Contatos[f"{nome}"])
+
 while True:
     print("Menu principal")
-    print("1. ")
-    opcao += input("Opção: ")
+    print("1. Listar contatos.")
+    print("2. Pesquisar contato.")
+    print("3. Sair.")
+    print("")
+    
+    opcao = input("Opção: ")
+    if opcao == "1":
+        print("")
+        listar()
+        print("")
+    elif opcao == "2":
+        nome = input("Qual seria o nome? ")
+        print("")
+        pesquisar(nome)
+        print("")
+    else:
+        break
