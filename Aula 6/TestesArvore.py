385e7ce2e68eb6f5dc9bd6bb8d9b1efa690d6715
Dione Pinheiro <dionepinheiro6030@gmail.com>
2026-09-15 19:33:42 -0300

    Adicionar funções de travessia em ordem e pos-ordem na árvore

--- Aula 6/TestesArvore.py
+++ Aula 6/TestesArvore.py
@@ -22,6 +22,22 @@ def preOrdem(raiz):
         # 3. Vai para a subárvore direita
         preOrdem(raiz.dir)
 
+
+def emOrdem(raiz):
+    if raiz is not None:
+        # 1. olha o da esquerda primeiro
+        emOrdem(raiz.esq)
+        # 2. visita o node
+        print(raiz.valor, end=" ")
+        # 3. olha a direita no final
+        emOrdem(raiz.dir)
+
+def posOrdem(raiz):
+    if raiz is not None:
+        posOrdem(raiz.esq)
+        posOrdem(raiz.dir)
+        print(raiz.valor, end=" ")
+
 def main():
     valores = [10, 5, 2, 7, 15, 12, 20]
     arvore = None
@@ -32,6 +48,10 @@ def main():
         
     print("Travessia em Pré-ordem:")
     preOrdem(arvore)
+    print("\n Travessia em ordem:")
+    emOrdem(arvore)
+    print("\n Travessia Pos-ordem:")
+    posOrdem(arvore)
 
 if __name__ == "__main__":
     main()
